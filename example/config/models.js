39857bdaad44b0dd7c0efb0ae98c3e492c183746
onwd <onwdit@gmail.com>
2019-01-19 01:40:18 +0600

    Moved dataSource to model definition; added todo

--- example/config/models.js
+++ example/config/models.js
@@ -1,17 +1,15 @@
+// TODO: auto-discover models instead of defining them manually
 module.exports = [
   {
     "name": "File",
-    "dataSource": "db1",
     "definition": require("../models/file.json")
   },
   {
     "name": "Role",
-    "dataSource": "db1",
     "definition": require("../models/role.json")
   },
   {
     "name": "User",
-    "dataSource": "db2",
     "definition": require("../models/user.json")
   }
 ];
