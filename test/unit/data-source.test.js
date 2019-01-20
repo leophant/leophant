a15cb60c812bcb5355a0ba11fa956f47c60b7414
onwd <onwdit@gmail.com>
2019-01-20 20:33:45 +0600

    Update DataSource unit tests

--- test/unit/data-source.test.js
+++ test/unit/data-source.test.js
@@ -1,34 +1,36 @@
 const { expect }     = require('chai');
 const { DataSource } = require('../support');
 
+const validDataSourceDefinition = {
+  name:      'db',
+  connector: 'mysql',
+  host:      'localhost',
+  port:      5432,
+  database:  'db',
+  username:  'username',
+  password:  'password'
+};
+
+const dataSourceConstructor = (definition) => {
+  return () => {
+    new DataSource(definition);
+  };
+};
+
 describe('DataSource', () => {
   describe('connector is required', () => {
-    it('should throw error when connector is not supplied', () => {
-      expect(() => {
-        new DataSource({
-          "name": "db1",
-          // "connector": "mysql",
-          "host": "localhost",
-          "port": 5432,
-          "database": "db1",
-          "username": "username",
-          "password": "password"
-        });
-      }).to.throw(Error);
+    it('should throw error when unsupported connector is supplied', () => {
+      const definition = { ...validDataSourceDefinition, connector: 'unsupported' };
+      expect(dataSourceConstructor(definition)).to.throw(Error);
     });
 
-    it('should work when connector is supplied', () => {
-      expect(() => {
-        new DataSource({
-          "name": "db1",
-          "connector": "mysql",
-          "host": "localhost",
-          "port": 5432,
-          "database": "db1",
-          "username": "username",
-          "password": "password"
-        });
-      }).not.to.throw(Error);
+    it('should work when supported connector is supplied', () => {
+      const supportedConnectors = ['mysql', 'postgresql'];
+
+      for(const connector of supportedConnectors) {
+        const definition = { ...validDataSourceDefinition, connector };
+        expect(dataSourceConstructor(definition)).not.to.throw(Error);
+      }
     });
   });
 });
