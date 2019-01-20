5970a4ddf8564cfb8ac8af477a9c4c9044b24ce5
onwd <onwdit@gmail.com>
2019-01-20 21:53:58 +0600

    config/models.js now exports a list of model definitions

--- example/config/models.js
+++ example/config/models.js
@@ -1,15 +1,6 @@
 // TODO: auto-discover models instead of defining them manually
 module.exports = [
-  {
-    "name": "File",
-    "definition": require("../models/file.json")
-  },
-  {
-    "name": "Role",
-    "definition": require("../models/role.json")
-  },
-  {
-    "name": "User",
-    "definition": require("../models/user.json")
-  }
+  require('../models/file.json'),
+  require('../models/role.json'),
+  require('../models/user.json')
 ];
